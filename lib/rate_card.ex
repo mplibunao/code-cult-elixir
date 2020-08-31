1fb946693c9736e10eb0c8b0ae388c72c996d9b0
MP <markpaololibunao@gmail.com>
2020-09-01 04:00:33 +0800

    Uses pattern-matching plus guard to make the tests passing

--- lib/rate_card.ex
+++ lib/rate_card.ex
@@ -1,5 +1,15 @@
 defmodule RateCard do
-  def get_pricing_for(vehicle, distance) do
-    # update code
-  end
+  def get_pricing_for(_, distance) when distance < 0, do: {:error, :invalid_distance}
+
+  def get_pricing_for("bike", distance) when distance < 5, do: {:ok, 15}
+  def get_pricing_for("bike", distance) when distance < 10, do: {:ok, 17}
+  def get_pricing_for("bike", distance) when distance < 15, do: {:ok, 22}
+  def get_pricing_for("bike", distance) when distance < 20, do: {:ok, 27}
+  def get_pricing_for("bike", distance) when distance >= 20, do: {:ok, 30}
+
+  def get_pricing_for("car", distance) when distance < 5, do: {:ok, 20}
+  def get_pricing_for("car", distance) when distance < 10, do: {:ok, 25}
+  def get_pricing_for("car", distance) when distance < 15, do: {:ok, 30}
+  def get_pricing_for("car", distance) when distance < 20, do: {:ok, 35}
+  def get_pricing_for("car", distance) when distance >= 20, do: {:ok, 40}
 end
